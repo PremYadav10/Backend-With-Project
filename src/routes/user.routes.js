9117fe596ae54c3effbc8c05211c7d7a349ebae2
PremYadav10 <yadavprem9993@gmail.com>
2025-09-06 16:14:44 +0530

    add user channel profile and watch history retrieval functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword,
+     getCurrentUser,
+     getUserChannelProfile,
+      getWatchHistory, loginUser,
+       logoutUser, refreshAccessToken,
+     registerUser, updateAcoountDetails,
+      updateUserAvatar, updateUserCoverImage 
+    } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,6 +30,21 @@ router.route("/login").post(loginUser)
 
 //secured route
 router.route("/logout").post(varifyJWT,logoutUser)
+
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(varifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(varifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(varifyJWT,updateAcoountDetails)
+
+router.route("/avatar").patch(varifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(varifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(varifyJWT,getUserChannelProfile)
+
+router.route("/history").get(varifyJWT,getWatchHistory)
 
 export default router;
